da489af1f4f9356d1b8918a97f7bb51a7ab304e9
Dominique Grisoni <misanthrope20218@gmail.com>
2024-10-19 16:19:25 +0200

    Create the crud for products

--- api/src/routes/products/productController.ts
+++ api/src/routes/products/productController.ts
@@ -1,28 +1,85 @@
 import { Request, Response } from 'express';
+import { db } from '../../db/index';
+import { productsTable } from '../../db/productsSchema';
+import { eq } from 'drizzle-orm';
 
 // Recupérer la liste des produits
-export function listProducts(req: Request, res: Response) {
-  res.send('Liste des produits');
+export async function listProducts(req: Request, res: Response) {
+  try {
+    const products = await db.select().from(productsTable);
+    res.json(products);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 }
 
 // Recupérer un produit par son id
-export function getProductById(req: Request, res: Response) {
-  res.send(`Produit ${req.params.id}`);
+export async function getProductById(req: Request, res: Response) {
+  const id = Number(req.params.id);
+  try {
+    const [product] = await db
+      .select()
+      .from(productsTable)
+      .where(eq(productsTable.id, id));
+
+    if (!product || product === undefined) {
+      res.status(404).send('Aucun produit trouvé');
+    } else {
+      res.status(200).json(product);
+    }
+  } catch (error) {
+    res.status(500).send(error);
+  }
 }
 
 // Créer un produit
-export function createProduct(req: Request, res: Response) {
-  console.log(req.body);
-
-  res.send("Création d'un produit ici");
+export async function createProduct(req: Request, res: Response) {
+  try {
+    const [product] = await db
+      .insert(productsTable)
+      .values(req.body)
+      .returning();
+    res.status(201).json(product);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 }
 
-// Mettre à jour un produit
-export function updateProduct(req: Request, res: Response) {
-  res.send(`Mise à jour du produit ${req.params.id}`);
+// Mettre à jour un produit par son id
+export async function updateProduct(req: Request, res: Response) {
+  const id = Number(req.params.id);
+  try {
+    const updatedFields = req.body;
+
+    const [updatedProduct] = await db
+      .update(productsTable)
+      .set(updatedFields)
+      .where(eq(productsTable.id, id))
+      .returning();
+    if (!updatedProduct || updatedProduct === undefined) {
+      res.status(404).send('Aucun produit trouvé');
+    } else {
+      res.status(200).json(updatedProduct);
+    }
+  } catch (error) {
+    res.status(500).send(error);
+  }
 }
 
-// Supprimer un produit
-export function deleteProduct(req: Request, res: Response) {
-  res.send(`Suppression du produit ${req.params.id}`);
+// Supprimer un produit par son id
+export async function deleteProduct(req: Request, res: Response) {
+  const id = Number(req.params.id);
+  try {
+    const [deletedProduct] = await db
+      .delete(productsTable)
+      .where(eq(productsTable.id, id))
+      .returning();
+    if (!deletedProduct || deletedProduct === undefined) {
+      res.status(404).send('Aucun produit trouvé');
+    } else {
+      res.status(200).json(deletedProduct);
+    }
+  } catch (error) {
+    res.status(500).send(error);
+  }
 }
