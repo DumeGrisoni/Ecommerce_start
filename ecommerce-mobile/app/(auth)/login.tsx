34e9958b4bea71eba176eb4233d5645ac4dd27f0
Dominique Grisoni <misanthrope20218@gmail.com>
2024-11-14 13:42:44 +0100

    add validation and error to register and login

--- ecommerce-mobile/app/(auth)/login.tsx
+++ ecommerce-mobile/app/(auth)/login.tsx
@@ -1,3 +1,4 @@
+import { login } from '@/api/auth';
 import { Box } from '@/components/ui/box';
 import { Button, ButtonText } from '@/components/ui/button';
 import { FormControl } from '@/components/ui/form-control';
@@ -5,6 +6,7 @@ import { Heading } from '@/components/ui/heading';
 import { Input, InputField, InputIcon, InputSlot } from '@/components/ui/input';
 import { Text } from '@/components/ui/text';
 import { VStack } from '@/components/ui/vstack';
+import { useMutation } from '@tanstack/react-query';
 import { Link, Stack } from 'expo-router';
 import { EyeIcon, EyeOffIcon } from 'lucide-react-native';
 import React from 'react';
@@ -13,6 +15,16 @@ import { Platform } from 'react-native';
 export default function Login() {
   // ------------ State -----------------
   const [showPassword, setShowPassword] = React.useState(false);
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  // ------------ Mutations -----------------
+
+  const loginMutation = useMutation({
+    mutationFn: () => login(email, password),
+    onSuccess: () => console.log('Success'),
+    onError: () => console.log('Error'),
+  });
 
   // ------------ Constants -----------------
 
@@ -31,20 +43,26 @@ export default function Login() {
         options={{ title: 'Connexion', headerTitleAlign: 'center' }}
       />
       <FormControl
+        isInvalid={loginMutation.isError}
         className={`border ${isWeb ? 'lg:w-[20%] md:w-[50%] w-[90%]' : 'w-full'} bg-typography-white h-auto rounded-lg border-outline-300`}
       >
         <VStack space="xl" className="p-4">
           <Heading className="text-typography-900">S'identifier</Heading>
+
           <VStack space="xs">
             <Text className="text-typography-500">Votre e-mail</Text>
             <Input>
-              <InputField type="text" />
+              <InputField type="text" value={email} onChangeText={setEmail} />
             </Input>
           </VStack>
           <VStack space="xs">
             <Text className="text-typography-500 leading-1">Mot de passe</Text>
             <Input className="text-center">
-              <InputField type={showPassword ? 'text' : 'password'} />
+              <InputField
+                type={showPassword ? 'text' : 'password'}
+                value={password}
+                onChangeText={setPassword}
+              />
               <InputSlot className="pr-3" onPress={handleState}>
                 {/* EyeIcon, EyeOffIcon are both imported from 'lucide-react-native' */}
                 <InputIcon
@@ -54,18 +72,21 @@ export default function Login() {
               </InputSlot>
             </Input>
           </VStack>
+          {loginMutation.isError && (
+            <Text className="text-error-500 text-center">
+              Veuillez vérifier vos identifiants
+            </Text>
+          )}
           <Button
             className="mx-auto w-full"
-            onPress={() => {
-              console.log('Login');
-            }}
+            onPress={() => loginMutation.mutate()}
           >
             <ButtonText className="text-typography-0">Connexion</ButtonText>
           </Button>
         </VStack>
         <Text className="text-typography-500 text-center my-4">
           Vous n'avez pas de compte ?{' '}
-          <Link href="/signIn" className="text-primary-500">
+          <Link href="/register" className="text-primary-500">
             Inscrivez-vous
           </Link>
         </Text>
--- ecommerce-mobile/app/(auth)/register.tsx
+++ ecommerce-mobile/app/(auth)/register.tsx
@@ -1,3 +1,4 @@
+import { register } from '@/api/auth';
 import { Box } from '@/components/ui/box';
 import { Button, ButtonText } from '@/components/ui/button';
 import { FormControl } from '@/components/ui/form-control';
@@ -6,14 +7,22 @@ import { HStack } from '@/components/ui/hstack';
 import { Input, InputField, InputIcon, InputSlot } from '@/components/ui/input';
 import { Text } from '@/components/ui/text';
 import { VStack } from '@/components/ui/vstack';
+import { useMutation } from '@tanstack/react-query';
 import { Link, Stack } from 'expo-router';
 import { EyeIcon, EyeOffIcon } from 'lucide-react-native';
 import React from 'react';
-import { Platform, ScrollView } from 'react-native';
+import { Alert, Platform, ScrollView } from 'react-native';
 
-export default function SignIn() {
+export default function Register() {
   // ------------ State -----------------
   const [showPassword, setShowPassword] = React.useState(false);
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [name, setName] = React.useState('');
+  const [surname, setSurname] = React.useState('');
+  const [adress, setAdress] = React.useState('');
+  const [postalCode, setPostalCode] = React.useState('');
+  const [city, setCity] = React.useState('');
 
   // ------------ Constants -----------------
 
@@ -24,6 +33,25 @@ export default function SignIn() {
     setShowPassword(!showPassword);
   };
 
+  // ------------ Mutations -----------------
+
+  const registerMutation = useMutation({
+    mutationFn: () =>
+      register(
+        email,
+        password,
+        name,
+        surname,
+        adress,
+        Number(postalCode),
+        city
+      ),
+    onSuccess: () => console.log('Success register'),
+    onError: (err) => {
+      console.log(err);
+    },
+  });
+
   return (
     <ScrollView>
       <Box className="flex-1 items-center justify-center w-full h-full p-6">
@@ -31,6 +59,7 @@ export default function SignIn() {
           options={{ title: 'Inscription', headerTitleAlign: 'center' }}
         />
         <FormControl
+          isInvalid={registerMutation.isError}
           className={`border ${isWeb ? 'lg:w-[30%] md:w-[50%] w-[90%]' : 'w-full'} bg-typography-white h-auto rounded-lg border-outline-300`}
         >
           <VStack space="xl" className="p-4">
@@ -38,19 +67,27 @@ export default function SignIn() {
             <VStack space="xs">
               <Text className="text-typography-500">Votre adresse e-mail</Text>
               <Input>
-                <InputField type="text" />
+                <InputField type="text" value={email} onChangeText={setEmail} />
               </Input>
               <Text className="text-typography-500">Votre nom</Text>
               <Input>
-                <InputField type="text" />
+                <InputField
+                  type="text"
+                  value={surname}
+                  onChangeText={setSurname}
+                />
               </Input>
               <Text className="text-typography-500">Votre prenom</Text>
               <Input>
-                <InputField type="text" />
+                <InputField type="text" value={name} onChangeText={setName} />
               </Input>
               <Text className="text-typography-500">Votre adresse</Text>
               <Input>
-                <InputField type="text" />
+                <InputField
+                  type="text"
+                  value={adress}
+                  onChangeText={setAdress}
+                />
               </Input>
               {isWeb ? (
                 <HStack
@@ -60,13 +97,22 @@ export default function SignIn() {
                   <VStack space="xs" className="flex-1 ">
                     <Text className="text-typography-500">Code postal</Text>
                     <Input>
-                      <InputField type="text" />
+                      <InputField
+                        keyboardType="numeric"
+                        type="text"
+                        value={postalCode}
+                        onChangeText={setPostalCode}
+                      />
                     </Input>
                   </VStack>
                   <VStack space="xs" className="flex-1 md:flex-auto">
                     <Text className="text-typography-500">Ville</Text>
                     <Input>
-                      <InputField type="text" />
+                      <InputField
+                        type="text"
+                        value={city}
+                        onChangeText={setCity}
+                      />
                     </Input>
                   </VStack>
                 </HStack>
@@ -74,12 +120,21 @@ export default function SignIn() {
                 <VStack space="xs">
                   <Text className="text-typography-500">Code postal</Text>
                   <Input>
-                    <InputField type="text" />
+                    <InputField
+                      keyboardType="numeric"
+                      type="text"
+                      value={postalCode}
+                      onChangeText={setPostalCode}
+                    />
                   </Input>
 
                   <Text className="text-typography-500">Ville</Text>
                   <Input>
-                    <InputField type="text" />
+                    <InputField
+                      type="text"
+                      value={city}
+                      onChangeText={setCity}
+                    />
                   </Input>
                 </VStack>
               )}
@@ -89,7 +144,11 @@ export default function SignIn() {
                 Mot de passe
               </Text>
               <Input className="text-center">
-                <InputField type={showPassword ? 'text' : 'password'} />
+                <InputField
+                  type={showPassword ? 'text' : 'password'}
+                  value={password}
+                  onChangeText={setPassword}
+                />
                 <InputSlot className="pr-3" onPress={handleState}>
                   {/* EyeIcon, EyeOffIcon are both imported from 'lucide-react-native' */}
                   <InputIcon
@@ -99,10 +158,27 @@ export default function SignIn() {
                 </InputSlot>
               </Input>
             </VStack>
+            {registerMutation.isError && (
+              <Text className="text-error-500 text-center">
+                Veuillez vérifier vos identifiants
+              </Text>
+            )}
             <Button
               className="mx-auto w-full my-2"
               onPress={() => {
-                console.log('Login');
+                if (
+                  email === '' ||
+                  password === '' ||
+                  name === '' ||
+                  surname === '' ||
+                  adress === '' ||
+                  postalCode === '' ||
+                  city === ''
+                ) {
+                  Alert.alert('Erreur', 'Veuillez renseigner tous les champs');
+                } else {
+                  registerMutation.mutate();
+                }
               }}
             >
               <ButtonText className="text-typography-0">Continuer</ButtonText>
