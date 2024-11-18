acc099b842cd814e7bb63019225f1e5e3a666d4a
Dominique Grisoni <misanthrope20218@gmail.com>
2024-11-18 11:17:21 +0100

    Add searchBar to main screen

--- ecommerce-mobile/app/index.tsx
+++ ecommerce-mobile/app/index.tsx
@@ -1,14 +1,24 @@
-import { ActivityIndicator, FlatList, View } from 'react-native';
-import React, { useEffect } from 'react';
+import {
+  ActivityIndicator,
+  FlatList,
+  Pressable,
+  TextInput,
+  View,
+} from 'react-native';
+import React from 'react';
+
+// ------------ Imports Personnelles -----------------
 import ProductListItem from '../components/ProductListItem';
 import { listProducts } from '@/api/products';
 import { useBreakpointValue } from '@/components/ui/utils/use-break-point-value';
 import { useQuery } from '@tanstack/react-query';
 import { Text } from '@/components/ui/text/index.web';
-import { useRouter } from 'expo-router';
+import { ProductType } from '@/types/types';
+import { CloseIcon, Icon, SearchIcon } from '@/components/ui/icon';
+import { HStack } from '@/components/ui/hstack';
 
 export default function HomeScreen() {
-  const router = useRouter();
+  const [searchQuery, setSearchQuery] = React.useState('');
   const { data, isLoading, error } = useQuery({
     queryKey: ['products'],
     queryFn: listProducts,
@@ -16,6 +26,10 @@ export default function HomeScreen() {
 
   // Changer ici le nombre de colonnes en fonction de la largeur de l'écran
   const numColumns = useBreakpointValue({ default: 2, sm: 3, lg: 4 });
+
+  const filteredData = data?.filter((product: ProductType) =>
+    product.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
 
   if (isLoading) {
     <View className=" flex-1 h-full w-full items-center justify-center">
@@ -30,14 +44,31 @@ export default function HomeScreen() {
   }
 
   return (
-    <FlatList
-      key={numColumns}
-      data={data}
-      numColumns={numColumns}
-      contentContainerClassName="gap-2 max-w-[960px] mx-auto w-full"
-      columnWrapperClassName="gap-2 mx-2"
-      className="my-2 flex-1 "
-      renderItem={({ item }) => <ProductListItem product={item} />}
-    />
+    <>
+      <HStack className="justify-between w-[90%] mx-auto items-center border border-gray-300 rounded mt-4 mb-2">
+        <HStack className="bg-white rounded p-2 gap-2 w-full justify-start items-center">
+          <Icon as={SearchIcon} size="xl" color="gray" />
+          <TextInput
+            placeholder="Rechercher un produit"
+            value={searchQuery}
+            onChangeText={setSearchQuery}
+          />
+          {searchQuery.length > 0 && (
+            <Pressable onPress={() => setSearchQuery('')} className="ml-auto">
+              <Icon as={CloseIcon} size="xl" color="gray" />
+            </Pressable>
+          )}
+        </HStack>
+      </HStack>
+      <FlatList
+        key={numColumns}
+        data={filteredData}
+        numColumns={numColumns}
+        contentContainerClassName="gap-2 max-w-[960px] mx-auto w-full"
+        columnWrapperClassName="gap-2 mx-2"
+        className="my-2 flex-1 "
+        renderItem={({ item }) => <ProductListItem product={item} />}
+      />
+    </>
   );
 }
